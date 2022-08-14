844792b3a971969a4be9000b5a7daf98470c10f5
ali <alihamzehpour2002@gail.com>
2022-08-14 12:59:05 +0430

    Create user schema

--- routes/models/user.js
+++ routes/models/user.js
@@ -14,5 +14,33 @@ const userSchema = new mongoose.Schema({
         minlength: 2,
         maxlength: 50
     },
-    
-})
+    age:{
+        type: Number,
+        min: 10,
+        max: 200
+    },
+    email:{
+        type: String,
+        required: true,
+        uniquie: true,
+        minlength: 5,
+        maxlength: 255,
+    },
+    phoneNumber:{
+        type: String,
+        required: true,
+        validate:{
+            validator: function(val){
+                return val.length === 11
+            },
+            message: "Phone number should have 11 characters"
+        }
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    }
+});
+
