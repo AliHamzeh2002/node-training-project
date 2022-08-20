ec526777099bbeff63a10bdcae8640ec3a57ba5d
ali <alihamzehpour2002@gail.com>
2022-08-20 11:35:26 +0430

    Add like validation

--- models/like.js
+++ models/like.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
+Joi.objectId = require('joi-objectid')(Joi)
 
 const likeSchema = new mongooseSchema({
     post:{
@@ -16,3 +17,13 @@ const likeSchema = new mongooseSchema({
 
 const Like = mongoose.model("Like", likeSchema);
 
+function validateLike(like){
+    const schema = Joi.object({
+        post: joi.objectId().required(),
+        user: joi.objectId().required(),
+    })
+    return schema.validate(like);
+}
+
+exports.Like = Like;
+exports.validate = validateLike
